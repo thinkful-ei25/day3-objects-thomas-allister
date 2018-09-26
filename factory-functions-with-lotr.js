c81b96bb8b52eb31c26d0a118c72e03fef4dc690
Allister Rooke <allisterg@gmail.com>
2018-09-26 16:51:01 -0400

    finished factory functions with lotr

--- factory-functions-with-lotr.js
+++ factory-functions-with-lotr.js
@@ -1,23 +1,49 @@
 'use strict';
 
-function createCharacter(Name, Nickname, Race, Origin, Attack, Defense) {
+function createCharacter(name, nickName, race, origin, attack, defense, weapon) {
   return {
-    Name: Name,
-    Nickname,
-    Race,
-    Origin,
-    Attack,
-    Defense,
+    name,
+    nickName,
+    race,
+    origin,
+    attack,
+    defense,
+    weapon,
     describe: function() {
-      console.log(`${this.Name} is a ${this.Race} from ${this.Origin}.`);
+      console.log(`${this.name} is a ${this.race} from ${this.origin} who uses ${this.weapon}.`);
     },
     evaluateFight: function(charObj) {
-      return `Your opponent takes ${((this.Attack - charObj.Defense > 0) ? this.Attack - charObj.Defense : 0)} damage and you receive ${((charObj.Attack - this.Defense > 0) ? charObj.Attack - this.Defense : 0)} damage`
+      return `Your opponent takes ${((this.attack - charObj.defense > 0) ? this.attack - charObj.defense : 0)} damage and you receive ${((charObj.attack - this.defense > 0) ? charObj.attack - this.defense : 0)} damage`
     }
   };
 }
 
-let gandalf = createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6);
-let bilbo = createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1);
+let gandalf = createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6, 'a wizard staff');
+let bilbo = createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1, 'the Ring');
 gandalf.describe();
 console.log(gandalf.evaluateFight(bilbo));
+
+const characters = [
+  createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6, 'a wizard staff'),
+  createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1, 'the Ring'),
+  createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2, 'String and Barrow Blade'),
+  createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8, 'Anduril'),
+  createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5, 'Bow and Arrow')
+];
+
+characters.push(createCharacter('Arwen Undomiel', 'arwen', 'Half-Elf', 'Rivendell', 5, 5, 'Hadhafang'));
+let aragorn = characters.find(function(character) {
+  return character.nickName === 'aragorn';
+} );
+aragorn.describe();
+
+const hobbits = characters.filter(function(character) {
+  return character.race === 'Hobbit';
+});
+console.log(hobbits);
+
+const highAttack = characters.filter(function(character) {
+  return character.attack > 5;
+});
+console.log(highAttack);
+
