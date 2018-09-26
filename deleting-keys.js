fa5bf18563df7bc8b40b14e2da9e695505e3cba3
Thomas Chun <tchun2055@gmail.com>
2018-09-26 09:08:04 -0700

    added deleting-keys.js

---
+++ deleting-keys.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
