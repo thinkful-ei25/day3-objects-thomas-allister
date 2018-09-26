52d78fc7022fdf10fc0d4991110cdac4bda1fc83
Thomas Chun <tchun2055@gmail.com>
2018-09-26 08:54:43 -0700

    added object creator

---
+++ object-drills.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Object creator
+
+function createMyObject() {
+  const obj = {
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: () => 'hello'
+  };
+  return obj;
+}
+
